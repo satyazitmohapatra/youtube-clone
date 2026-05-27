b95eaa78ebfef2cfe3d0d2c7235a11d75a540e75
Satyajit Mohapatra <overfitsatyazit@gmail.com>
2026-05-27 15:49:45 +0530

    host changed

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -8,7 +8,7 @@ const options = {
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Host': 'youtube-data-api-v33.p.rapidapi.com',
   },
 };
 
